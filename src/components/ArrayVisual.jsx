61425df683c3d10b3c6ffdc069c595a0f7c0248c
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-28 18:58:19 +0530

    add dark mode support to ArrayVisual component; adjust text and background colors based on isDark prop

--- src/components/ArrayVisual.jsx
+++ src/components/ArrayVisual.jsx
@@ -1,14 +1,14 @@
 import { motion } from "framer-motion";
 
-const ArrayVisual = ({ data, highlightIndices = [], controls }) => {
+const ArrayVisual = ({ data, highlightIndices = [], controls, isDark }) => {
   return (
     <div className="flex space-x-2 mt-4">
       {data.map((num, idx) => (
         <motion.div
           key={idx}
           custom={idx}
-          className={`w-10 h-10 flex items-center justify-center rounded-md text-white font-bold ${
-            highlightIndices.includes(idx) ? "bg-green-500" : "bg-indigo-600"
+          className={`w-10 h-10 flex items-center justify-center rounded-md ${isDark ? "text-white" : "text-gray-800"} font-bold ${
+            highlightIndices.includes(idx) ? (isDark ? "bg-green-500" : "bg-pink-300") : (isDark ? "bg-indigo-600" : "bg-purple-200")
           }`}
           animate={controls}
           transition={{ duration: 0.3 }}
