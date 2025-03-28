b6b9d6f1a591177ffafc7f39ec2175e5934388fa
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-28 18:58:24 +0530

    add dark mode support to Footer component; adjust text color based on isDark prop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,11 @@
+const Footer = ({ isDark }) => {
+    return (
+      <footer className={`mt-16 ${isDark ? "text-gray-400" : "text-gray-600"}`}>
+        <p> Made with ❤️ | © 2025</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+
 import { motion } from "framer-motion";
-
-const Footer = () => {
-  return (
-    <motion.footer
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1.5, duration: 1 }}
-      className="mt-36 text-gray-400 text-sm flex sticky bottom-0"
-    >
-      Made with ❤️ | © 2025
-    </motion.footer>
-  );
-};
-
-export default Footer;
